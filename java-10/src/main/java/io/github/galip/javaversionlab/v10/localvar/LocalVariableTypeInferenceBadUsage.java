6959b414de3d1ea208d2aa3d0c950d2d7be491f6
galip <galippala@gmail.com>
2026-02-27 21:34:10 +0300

    README, examples and comments improved  java-10.

--- java-10/src/main/java/io/github/galip/javaversionlab/v10/localvar/LocalVariableTypeInferenceBadUsage.java
+++ java-10/src/main/java/io/github/galip/javaversionlab/v10/localvar/LocalVariableTypeInferenceBadUsage.java
@@ -1,26 +1,41 @@
 package io.github.galip.javaversionlab.v10.localvar;
 
+import java.util.ArrayList;
+
 public class LocalVariableTypeInferenceBadUsage {
 
-    // Cannot use var without initializer
-    // var x;
+    public static void main(String[] args) {
 
-    // Cannot infer type from null
-    // var y = null;
+        // Cannot use var without initializer
+        // var x;
 
-    // Compound declarations not allowed
-    // var op1 = 1, op2 = 2;
+        // Cannot infer type from null
+        // var y = null;
 
-    // array initializer needs explicit target-type
-    // var array = {1,2,3}
+        // Compound declarations not allowed
+        // var op1 = 1, op2 = 2;
 
-    // not allowed in method parameter
-    // public void test(var param) {}
+        // array initializer needs explicit target-type
+        // var array = {1,2,3}
 
-    // not allowed in generics
-    // class Example<var T> {}
+        // not allowed in method parameter
+        // public void test(var param) {}
 
-    // avoid to use this - type is not obvious here
-    // var result = process();
+        // not allowed in generics
+        // class Example<var T> {}
 
+        // avoid to use this - type is not obvious here
+        // var result = process();
+
+        // Diamond parameter risk
+        var list = new ArrayList<>(); // compiler infers this as ArrayList<Object> list = new ArrayList<>();
+        list.add("hello");
+        list.add(10);
+
+        // We may think it is List<String> but it is Object
+        for (var item : list) {
+            String value = (String) item; // java.lang.ClassCastException: java.base/java.lang.Integer cannot be cast to java.base/java.lang.String
+            System.out.println(item);
+        }
+    }
 }
--- java-10/src/main/java/io/github/galip/javaversionlab/v10/localvar/README.md
+++ java-10/src/main/java/io/github/galip/javaversionlab/v10/localvar/README.md
@@ -8,17 +8,32 @@ Can we reduce verbosity without losing readability?
 -
 
 This feature allows variables to be declared without explicit type.
-It is inferred ar compile time.
+The compiler infers it at compile time.
+
+var message = "hello"; is compiled as
+
+String message = "hello";
+
+var is not dynamic typing.
 
 Where to use it?
 
 * For loops
 * Local variables
 * try-with-resources
+* Stream transformations
 
 Where not to use it?
 * Fields
 * Return types
 * Method parameters
 
-This is compile time feature. So, ZERO runtime impact.
+When the type is not obvious we should avoid to use it.
+
+Diamond operator is risky to use.
+
+var list = new ArrayList<>(); compiler infers this as ArrayList<Object> list = new ArrayList<>();
+We may think it is List<String> but it is Object. This causes a ClassCastException.
+
+
+This feature is a compile time feature. So, ZERO runtime impact.
