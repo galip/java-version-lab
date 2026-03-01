afd2ae4bd86261dba4d635e296ce05be07eb555e
galip <galippala@gmail.com>
2026-03-01 23:48:14 +0300

    - unmodifiable collections demo, copyOd & toUnmodifiable - list.of(), Map.of() - UnsupportedOperationException behavior

---
+++ java-10/src/main/java/io/github/galip/javaversionlab/v10/collections/UnmodifiableCollectionsDemo.java
@@ -0,0 +1,61 @@
+package io.github.galip.javaversionlab.v10.collections;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class UnmodifiableCollectionsDemo {
+    public static void main(String args[]) {
+
+        List<String> originalList = new ArrayList<>();
+        originalList.add("Java");
+        originalList.add("Go");
+
+        List<String> immutableCopyList = List.copyOf(originalList);
+        System.out.println("Immutable copy list: " + immutableCopyList);
+
+        // immutableCopyList.add("C++"); // UnsupportedOperationException
+
+        List<String> users = Arrays.asList("Mehmet", "Zeynep", "Ayşe")
+                        .stream()
+                        .map(String::toUpperCase)
+                        .collect(Collectors.toUnmodifiableList());
+
+        System.out.println("Unmodifiable stream: " + users);
+
+        // users.add("XYZ"); // UnsupportedOperationException
+
+        Map<String, String> bookMap = Map.of(
+                "Fathers And Sons","Turgenyev",
+                "The Lily Of The Valley", "Balzac"
+        );
+
+        // bookMap.put("book3", "author3"); // UnsupportedOperationException
+
+        Map<String, String> immutablemap = Map.copyOf(bookMap);
+        System.out.println("Immutable map: " + immutablemap);
+
+        // immutablemap.put("book3", "author3"); // UnsupportedOperationException
+
+
+        Map<String, String> modifiable = new HashMap<>();
+        modifiable.put("A", "X");
+
+        Map<String, String> unmodifiableView = Collections.unmodifiableMap(modifiable);
+        System.out.println("Before change: " + unmodifiableView);
+
+        modifiable.put("B", "Y");
+        System.out.println("After change: " + unmodifiableView);
+
+
+        // unmodifiableView.put("C", "V"); // UnsupportedOperationException
+
+    }
+}
+
+/*
+Immutable copy list: [Java, Go]
+Unmodifiable stream: [MEHMET, ZEYNEP, AYŞE]
+Immutable map: {The Lily Of The Valley=Balzac, Fathers And Sons=Turgenyev}
+Before change: {A=X}
+After change: {A=X, B=Y}
+*/
